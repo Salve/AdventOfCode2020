8880fa3176925b7726e4b7ec97c4fcd321145dae
Salve Spinnangr <salve@spinnangr.no>
2020-12-04 21:27:58 +0100

    Brute force is always the answer!

--- Day1/day1.go
+++ Day1/day1.go
@@ -10,7 +10,40 @@ import (
 
 func main() {
 	values := readExpenseReport("input")
-	fmt.Printf("Num values: %v", len(values))
+	part1(values)
+	part2(values)
+}
+
+func part1(values []int) {
+	for i, v1 := range values {
+		for j, v2 := range values {
+			if i == j {
+				continue
+			}
+			if v1+v2 == 2020 {
+				fmt.Printf("Part1: %v + %v = 2020, %v * %v = %v\n",
+					v1, v2, v1, v2, v1*v2)
+				return
+			}
+		}
+	}
+}
+
+func part2(values []int) {
+	for i, v1 := range values {
+		for j, v2 := range values {
+			for k, v3 := range values {
+				if i == j || i == k || j == k {
+					continue
+				}
+				if v1+v2+v3 == 2020 {
+					fmt.Printf("Part2: %v + %v + %v = 2020, %v * %v * %v = %v\n",
+						v1, v2, v3, v1, v2, v3, v1*v2*v3)
+					return
+				}
+			}
+		}
+	}
 }
 
 func readExpenseReport(filename string) (report []int) {
